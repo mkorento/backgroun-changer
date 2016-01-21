dc2e62dfadc317f4e0b36d494281b212c0afb912
Mika Korento <korento.mika@gmail.com>
2016-01-21 14:47:22 +0200

    Add build script

---
+++ build.ps1
@@ -0,0 +1,18 @@
+If (Test-Path .\dist){
+    rm .\dist -r -Force;
+}
+If (Test-Path .\build){
+    rm .\build -r -Force;
+}
+
+pyinstaller --onedir client.py
+mkdir .\dist\client\images;
+mkdir .\dist\client\icons;
+mkdir .\dist\client\config;
+cp .\images\1d6e3da17be1d926443c4f5bd63d342c.jpg .\dist\client\images\;
+cp .\config\last_completion.txt .\dist\client\config\;
+cp .\config\subreddits.txt .\dist\client\config\;
+cp .\icons\icon_busy.ico .\dist\client\icons;
+cp .\icons\icon_ready.ico .\dist\client\icons;
+cp .\server.py .\dist\client;
+cp .\Start.py .\dist\client;
